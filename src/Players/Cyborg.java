64cad21a164e88635e42a837aa4245dce7d8630b
LeventeWolf <wlevi2002@gmail.com>
2022-04-02 20:27:53 +0200

    Create Cyborg

---
+++ src/Players/Cyborg.java
@@ -0,0 +1,12 @@
+package Players;
+
+public class Cyborg extends Player{
+    public Cyborg(String name) {
+       super(name);
+    }
+
+    public void playTurn(){
+        System.out.println("Cyborg's turn!");
+    };
+
+}
